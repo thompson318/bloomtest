b7627b68c7b21c57339c78144fe6aedf8e4a0b65
Stephen Thompson <s.thompson@ucl.ac.uk>
2022-05-27 12:12:58 +0100

    Added water sound

---
+++ scripts/sounds.js
@@ -0,0 +1,17 @@
+const water = document.getElementById('watersound');
+water.volume = 0.90;
+water.play()
+
+//let x = 0
+//const soundFiles = 2
+//$("#narration").bind("ended", function(){
+//  x = x + 1
+//  const file = "sounds/" + String(x%soundFiles).padStart(2, '0') + ".mp3"
+//  console.log("Looking for ", file)
+//  narrator.src = file
+//  //should also wait for a random number of seconds
+//  narrator.load()
+//  narrator.play()
+//});
+
+
