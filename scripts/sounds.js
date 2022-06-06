08c3246c639543f831229c2700f792a65d4bbcd9
Stephen Thompson <s.thompson@ucl.ac.uk>
2022-06-06 15:09:15 +0100

    Fixed play aspect ratio and added sounds

--- scripts/sounds.js
+++ scripts/sounds.js
@@ -1,6 +1,12 @@
 const water = document.getElementById('watersound');
+const narration = document.getElementById('narration');
 const startButton = document.getElementById('startbutton');
 water.volume = 0.90;
+narration.volume = 0.30;
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
 
 function startplay(){
   console.log('Pressed the button');
@@ -8,6 +14,9 @@ function startplay(){
     .then(() => {
       clearInterval(playAttempt);
       startButton.style.visibility = 'hidden';
+      const delay_ms = (Math.round(Math.random() * 10)/2 + 5) * 1000
+      console.log('going to wait for ', delay_ms, 'ms') 
+      delay(delay_ms).then(() => narration.play());
     })
     .catch(error => {
       console.log('Unable to play the sound, User has not interacted yet.');
@@ -19,7 +28,10 @@ let playAttempt = setInterval(() => {
   water.play()
     .then(() => {
       clearInterval(playAttempt);
-      startButton.display = False
+      startButton.style.visibility = 'hidden';
+      const delay_ms = (Math.round(Math.random() * 10)/2 + 5) * 1000
+      console.log('going to wait for ', delay_ms, 'ms') 
+      delay(delay_ms).then(() => narration.play());
     })
     .catch(error => {
       console.log('Unable to play the sound, User has not interacted yet.');
@@ -27,16 +39,19 @@ let playAttempt = setInterval(() => {
 }, 2000);
 
 
-//let x = 0
-//const soundFiles = 2
-//$("#narration").bind("ended", function(){
-//  x = x + 1
-//  const file = "sounds/" + String(x%soundFiles).padStart(2, '0') + ".mp3"
-//  console.log("Looking for ", file)
-//  narrator.src = file
-//  //should also wait for a random number of seconds
-//  narrator.load()
-//  narrator.play()
-//});
+let x = 0
+const soundFiles = 43
+$("#narration").bind("ended", function(){
+  x = x + 1
+  const file = "sounds/" + String(x%soundFiles).padStart(2, '0') + "_panned.mp3"
+  console.log("Looking for ", file)
+  narration.src = file
+  narration.pause()
+  //should also wait for a random number of seconds
+  narration.load()
+  const delay_ms = (Math.round(Math.random() * 6)/2 + 1) * 1000
+  console.log('going to wait for ', delay_ms, 'ms') 
+  delay(delay_ms).then(() => narration.play());
+});
 
 
